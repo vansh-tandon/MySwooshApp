9aca581fb76c5a28fc41cb078f2e303b2edd54d7
vansh tandon <vanshtandon421@gmail.com>
2022-07-13 02:02:45 +0530

    lesson 2

---
+++ app/src/main/java/com/first/myswooshapp/league.kt
@@ -0,0 +1,11 @@
+package com.first.myswooshapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class league : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
