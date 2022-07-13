00fbd5f63b1e53cf5ef543d6a32be5b160cb33e6
vansh tandon <vanshtandon421@gmail.com>
2022-07-13 07:20:24 +0530

    lesson 3

--- app/src/main/java/com/first/myswooshapp/MainActivity.kt
+++ app/src/main/java/com/first/myswooshapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.first.myswooshapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var getStartedBtn:Button
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, league::class.java)
+            startActivity(leagueIntent)
+        }
+
+
+        }
+
+
     }
-}
